f35677cd5e8a005cb52051ea6728344909f0266d
jonas LE DUFF <jonasleduff14@gmail.com>
2025-09-12 18:09:47 +0200

    added all the other pieces

--- src/pieces/knight.py
+++ src/pieces/knight.py
@@ -1,3 +1,20 @@
 class Knight:
     def __init__(self, color):
         self.color = color
+        self.symbol = 'N' if color == 'white' else 'n'
+
+    def get_legal_moves(self, position, board):
+        moves = []
+        row, col = position
+        knight_moves = [
+            (2, 1), (2, -1), (-2, 1), (-2, -1),
+            (1, 2), (1, -2), (-1, 2), (-1, -2)
+        ]
+        
+        for dr, dc in knight_moves:
+            new_row, new_col = row + dr, col + dc
+            if 0 <= new_row < 8 and 0 <= new_col < 8:  # within board limits
+                if board[new_row][new_col] is None or board[new_row][new_col].color != self.color:
+                    moves.append((new_row, new_col))
+        
+        return moves
--- src/pieces/pawn.py
+++ src/pieces/pawn.py
@@ -1,3 +1,27 @@
 class Pawn:
     def __init__(self, color):
         self.color = color
+        self.symbol = 'P' if color == 'white' else 'p'
+
+    def get_legal_moves(self, position, board):
+        moves = []
+        row, col = position
+        direction = -1 if self.color == 'white' else 1  # white moves up, black moves down
+
+        # Move forward
+        if 0 <= row + direction < 8:
+            if board[row + direction][col] is None:  # empty square
+                moves.append((row + direction, col))
+                # First move can be two squares
+                if (self.color == 'white' and row == 6) or (self.color == 'black' and row == 1):
+                    if board[row + 2 * direction][col] is None:
+                        moves.append((row + 2 * direction, col))
+
+        # Capture diagonally
+        for dc in [-1, 1]:
+            if 0 <= col + dc < 8 and 0 <= row + direction < 8:
+                target_square = board[row + direction][col + dc]
+                if target_square is not None and target_square.color != self.color:
+                    moves.append((row + direction, col + dc))
+
+        return moves
