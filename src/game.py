2ae906f4a3bf9e798c59130f406893c60eb36b59
jonas LE DUFF <jonasleduff14@gmail.com>
2025-09-12 18:37:51 +0200

    refractor main

---
+++ src/game.py
@@ -0,0 +1,74 @@
+from board import Board
+from pieces.king import King
+
+class Game:
+    def __init__(self):
+        self.board = Board()
+        self.board.show()
+        self.player_turn = 'white'
+
+    def select_piece(self, position):
+        row, col = position
+        piece = self.board.board[row][col]
+        if piece is None:
+            print("No piece at the selected position.")
+            print(self.board.find_piece('k'))
+            return []
+        if piece.color != self.player_turn:
+            print(f"It's {self.player_turn}'s turn. Please select your own piece.")
+            return []
+        return piece.get_legal_moves(position, self.board.board)
+        
+    def switch_turn(self):
+        self.player_turn = 'black' if self.player_turn == 'white' else 'white'
+        print(f"It's now {self.player_turn}'s turn.")
+    
+
+    def run(self):
+        while True:
+            step_selectpiece = False
+            while not step_selectpiece:
+                user_input = input("Enter piece position (e.g., 'e2') or 'exit' to quit: ")
+                if user_input.lower() == 'exit' or user_input.lower() == 'quit':
+                    return
+                try:
+                    col = ord(user_input[0].lower()) - ord('a')
+                    row = 8 - int(user_input[1])
+                    print("in run ", col, ' ', row)
+                    moves = self.select_piece((row, col))
+                    print(f"Valid moves: {moves}")
+                    if(len(moves) > 0):
+                        step_selectpiece = True
+                        
+                except (ValueError):
+                    print("Invalid input. Please enter a valid position.", ValueError.args)
+
+            step_movepiece = False
+            while not step_movepiece:
+                user_input_move = input("Enter move position (e.g., 'e4'): ")
+                col_move = ord(user_input_move[0].lower()) - ord('a')
+                row_move = 8 - int(user_input_move[1])
+                if (row_move, col_move) in moves:
+                    piece = self.board.board[row][col]
+                    self.board.board[row_move][col_move] = piece
+                    self.board.board[row][col] = None
+                    self.board.show()
+                    if not self.checkWinner() == False:
+                        return
+
+                    step_movepiece = True
+                else:
+                    print("Invalid move.")
+            self.switch_turn()
+
+    
+    def checkWinner(self):
+        if self.board.find_piece(King('white')) is None:
+            print("Black wins!")
+            return 'black'
+        if self.board.find_piece(King('black')) is None:
+            print("White wins!")
+            return 'white'
+        return False
+self = App()
+self.run()
